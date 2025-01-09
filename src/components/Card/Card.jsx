1959d571e9bad04a2118d1f5bface854e1b7b09a
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-01-09 17:31:14 -0600

    Add hover animation to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 const Card = () => {
   return (
-    <div className="w-20 h-28 bg-black border-4 border-solid border-white rounded-lg text-white flex items-center justify-center">
+    <div className="w-20 h-28 bg-black border-4 border-solid border-white rounded-lg text-white flex items-center justify-center cursor-pointer hover:scale-110 transition">
       CARD
     </div>
   );
