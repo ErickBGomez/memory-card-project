54facc1d6cfe4b95a60d5638c77ca343fcc5acc7
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-01-09 17:28:54 -0600

    style card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 const Card = () => {
   return (
-    <div className="w-14 h-28 bg-black border-4 border-solid border-white text-white">
+    <div className="w-20 h-28 bg-black border-4 border-solid border-white rounded-lg text-white flex items-center justify-center">
       CARD
     </div>
   );
