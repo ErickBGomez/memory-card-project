499d2a9b70648642753622b1ae752d9d4802fb59
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-01-11 16:14:02 -0600

    Limit cards based on difficulty

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,17 +1,19 @@
 import Card from "../Card/Card";
 
-const Board = ({ difficulty = 1 }) => {
+const Board = ({ difficulty = 2 }) => {
   if (difficulty < 0 || difficulty > 2) return null;
 
   const emojis = ["🚀", "🏠", "🏢", "🏥", "🏦", "🏫", "🏭", "🏰", "💻", "📱"];
-  const cards = emojis.map((emoji, index) => <Card key={index} icon={emoji} />);
+  const cards = emojis
+    .map((emoji, index) => <Card key={index} icon={emoji} />)
+    .slice(0, 4 + difficulty * 2);
 
   return (
     <div
       // Apply max-w-96 class only when difficulty is 1 (3x2 board)
       className={`
       board flex items-center justify-center gap-4 flex-wrap
-      ${difficulty === 1 ? "max-w-80" : ""}
+      ${difficulty === 1 ? "max-w-80" : "max-w-96"}
       `}
     >
       {cards}
