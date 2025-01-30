8cbae2147274ac7c15d9f2673f9b6a0436cf84b2
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-01-30 17:53:18 -0600

    Style score feedback

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -2,10 +2,19 @@ const Score = ({ gameState }) => {
   const { score, highScore, phase } = gameState || {};
 
   return (
-    <div className="score">
-      <div className="score">Score: {score}</div>
-      <div className="high-score">High Score: {highScore}</div>
-      <div className="phase">Phase: {phase}</div>
+    <div className="score grid grid-cols-3 ">
+      <div className="score flex flex-col">
+        <div className="label font-bold text-xs">YOUR SCORE</div>
+        <div className="value">{score}</div>
+      </div>
+      <div className="phase flex flex-col items-center">
+        <div className="label font-bold text-xs">PHASE</div>
+        <div className="value">{phase}</div>
+      </div>
+      <div className="high-score flex flex-col items-end">
+        <div className="label font-bold text-xs">HIGH SCORE</div>
+        <div className="value">{highScore}</div>
+      </div>
     </div>
   );
 };
