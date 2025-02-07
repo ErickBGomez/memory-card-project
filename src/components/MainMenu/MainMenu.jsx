296a36abc2751b798f7db99ae99fb2c2cc4e2b5c
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-06 20:58:11 -0600

    Create animations with framer motion

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,23 +1,62 @@
+import { motion } from "motion/react";
+
 const MainMenu = ({ setDifficulty }) => {
   const selectDifficulty = (difficulty) => {
     setDifficulty(difficulty);
   };
 
   return (
-    <div className="main-menu flex flex-col gap-6">
-      <h1 className="text-5xl font-bold text-center text-white">MEMORY CARD</h1>
-      <p className="text-center">SELECT A DIFFICULTY</p>
-      <div className="flex flex-col justify-center gap-4">
-        <button className="primary" onClick={() => selectDifficulty(0)}>
+    <div className="main-menu">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
+        <h1 className="text-5xl font-bold text-center text-white">
+          MEMORY CARD
+        </h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
+        <p className="text-center mt-6 mb-6">SELECT A DIFFICULTY</p>
+      </motion.div>
+      <motion.div
+        className="flex flex-col justify-center gap-4"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+      >
+        <motion.button
+          className="primary"
+          onClick={() => selectDifficulty(0)}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 2 }}
+        >
           EASY
-        </button>
-        <button className="primary" onClick={() => selectDifficulty(1)}>
+        </motion.button>
+        <motion.button
+          className="primary"
+          onClick={() => selectDifficulty(1)}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 2.5 }}
+        >
           MEDIUM
-        </button>
-        <button className="primary" onClick={() => selectDifficulty(2)}>
+        </motion.button>
+        <motion.button
+          className="primary"
+          onClick={() => selectDifficulty(2)}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 3 }}
+        >
           HARD
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </div>
   );
 };
