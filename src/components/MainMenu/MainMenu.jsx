b404ca23f8e9a463f6a2aca9afb0a93450881756
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-06 21:05:35 -0600

    Animate buttons based on an array

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -5,6 +5,34 @@ const MainMenu = ({ setDifficulty }) => {
     setDifficulty(difficulty);
   };
 
+  const buttons = [
+    {
+      text: "EASY",
+      difficulty: 0,
+    },
+    {
+      text: "MEDIUM",
+      difficulty: 1,
+    },
+    {
+      text: "HARD",
+      difficulty: 2,
+    },
+  ];
+
+  const animatedButtons = buttons.map((button, index) => (
+    <motion.button
+      key={index}
+      onClick={() => setDifficulty(button.difficulty)}
+      className="primary"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.25, delay: 2 + index * 0.15 }}
+    >
+      {button.text}
+    </motion.button>
+  ));
+
   return (
     <div className="main-menu">
       <motion.div
@@ -23,40 +51,9 @@ const MainMenu = ({ setDifficulty }) => {
       >
         <p className="text-center mt-6 mb-6">SELECT A DIFFICULTY</p>
       </motion.div>
-      <motion.div
-        className="flex flex-col justify-center gap-4"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 1.5 }}
-      >
-        <motion.button
-          className="primary"
-          onClick={() => selectDifficulty(0)}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 2 }}
-        >
-          EASY
-        </motion.button>
-        <motion.button
-          className="primary"
-          onClick={() => selectDifficulty(1)}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 2.5 }}
-        >
-          MEDIUM
-        </motion.button>
-        <motion.button
-          className="primary"
-          onClick={() => selectDifficulty(2)}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 3 }}
-        >
-          HARD
-        </motion.button>
-      </motion.div>
+      <div className="flex flex-col justify-center gap-4">
+        {animatedButtons}
+      </div>
     </div>
   );
 };
