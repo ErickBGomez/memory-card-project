f4c6c80695f008471cd70813774e39118c645702
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2025-02-10 15:38:37 -0600

    TODO: Resolve bug of cards not updating

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,16 +5,18 @@ const Board = ({ gameState, clickCard }) => {
   const { difficulty, cards } = gameState || {};
   const [renderedCards, setRenderedCards] = useState({ content: [], index: 0 });
 
-  const mappedCards = cards?.map((card, index) => (
-    <Card
-      key={card._id || index}
-      url={card.url}
-      onClick={() => {
-        clickCard(card);
-        shuffleCards();
-      }}
-    />
-  ));
+  // TODO: Find a way to render cards and keep updates of the cards from gameLogic.js
+  // TODO: (this is because currently, renderedCards does not receive the new cards from gameLogic)
+  // const mappedCards = cards?.map((card, index) => (
+  //   <Card
+  //     key={card._id || index}
+  //     url={card.url}
+  //     onClick={() => {
+  //       clickCard(card);
+  //       shuffleCards();
+  //     }}
+  //   />
+  // ));
 
   // Shuffle cards in frontend to avoid creating new cards every click
   const shuffleCards = () => {
@@ -27,19 +29,31 @@ const Board = ({ gameState, clickCard }) => {
   // Delay rendering of cards to create a staggered effect
   useEffect(() => {
     const interval = setInterval(() => {
-      if (renderedCards.index >= mappedCards.length) {
+      if (renderedCards.index >= cards.length) {
         clearInterval(interval);
         return;
       }
 
+      const card = cards[renderedCards.index];
+
       setRenderedCards((prev) => ({
-        content: [...prev.content, mappedCards[prev.index]],
+        content: [
+          ...prev.content,
+          <Card
+            key={card._id || prev.index}
+            url={card.url}
+            onClick={() => {
+              clickCard(card);
+              shuffleCards();
+            }}
+          />,
+        ],
         index: prev.index + 1,
       }));
     }, 100);
 
     return () => clearInterval(interval);
-  }, [mappedCards, renderedCards.index]);
+  }, [cards, clickCard, renderedCards.index]);
 
   return (
     <div
