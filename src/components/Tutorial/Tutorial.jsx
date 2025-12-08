5668a636cee6c0ef0e49547d6c893c59c1abf221
ErickBGomez <erickbgomez.dev@gmail.com>
2025-12-07 18:40:47 -0600

    Fix: Step description was being erased from screen without exit animation

--- src/components/Tutorial/Tutorial.jsx
+++ src/components/Tutorial/Tutorial.jsx
@@ -1,43 +1,58 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { useContext, useState } from "react";
 import Card from "../Card/Card";
 import useGameCards from "../../hooks/useGameCards";
 import GameSettingsContext from "../../contexts/GameSettingsContext";
 import FloatingTextContext from "../../contexts/FloatingTextContext";
 
+const StepContainer = ({ children, stepKey }) => {
+  return (
+    <motion.div
+      className="w-full flex flex-col items-center text-[#e2e2e2] text-center text-sm"
+      key={stepKey}
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -10 }}
+      transition={{ duration: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
 const StepOne = () => {
   return (
-    <div>
+    <>
       <p>Cards will appear in your board</p>
       <p>Like these:</p>
-    </div>
+    </>
   );
 };
 
 const StepTwo = () => {
   return (
-    <div>
+    <>
       <p>You can select one card at the time</p>
       <p>Come on, click one!</p>
-    </div>
+    </>
   );
 };
 
 const StepThree = () => {
   return (
-    <div>
+    <>
       <p>But next one MUST NOT BE selected before</p>
       <p>Do you remember which one you selected?</p>
-    </div>
+    </>
   );
 };
 
 const StepFour = () => {
   return (
-    <div>
-      <p>In order to win, you must click all cards without repeating any</p>
+    <>
+      <p>To win, you must click all cards without repeating any</p>
       <p>Click the last one!</p>
-    </div>
+    </>
   );
 };
 
@@ -51,10 +66,8 @@ const Steps = {
 const Tutorial = () => {
   const [step, setStep] = useState(1);
   const [currentStepCompleted, setCurrentStepCompleted] = useState(true);
-  const [stepCardClicked, setStepCardClicked] = useState(null);
   const { setShowTutorial } = useContext(GameSettingsContext);
   const cards = useGameCards(3);
-  const [clickedCards, setClickedCards] = useState([]);
   const { createFloatingText } = useContext(FloatingTextContext);
 
   const handleNextStep = () => {
@@ -63,21 +76,34 @@ const Tutorial = () => {
     setCurrentStepCompleted(false);
   };
 
-  const handleCardClick = (clickEvent, cardId) => {
-    if (clickedCards.includes(cardId)) return;
+  const handleCardClick = (clickEvent, card) => {
     if (currentStepCompleted) return;
 
-    setClickedCards([...clickedCards, cardId]);
-
     if (step === 2) {
+      card.clicked = true;
       setCurrentStepCompleted(true);
-      setStepCardClicked(cardId);
+      createFloatingText(clickEvent, "✅");
+    }
+
+    if (step === 3 || step === 4) {
+      if (cards.find((c) => c._id === card._id).clicked) {
+        createFloatingText(clickEvent, "❌ Already clicked!");
+        return;
+      }
+
+      card.clicked = true;
+      setCurrentStepCompleted(true);
       createFloatingText(clickEvent, "✅");
     }
   };
 
   return (
-    <div className="max-w-[400px]">
+    <motion.div
+      className="max-w-[400px]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 0.5 }}
+    >
       {/* Title */}
       <motion.div
         initial={{ opacity: 0 }}
@@ -88,9 +114,18 @@ const Tutorial = () => {
       </motion.div>
 
       {/* Steps */}
-      <div className="w-full flex flex-col items-center text-[#e2e2e2]">
-        <div className="text-center">{Steps[step]}</div>
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          className="w-full flex flex-col items-center text-[#e2e2e2] text-center text-sm"
+          key={step}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          {Steps[step]}
+        </motion.div>
+      </AnimatePresence>
 
       {/* Cards */}
       <div className="w-full flex justify-center items-center gap-2 my-6">
@@ -99,10 +134,7 @@ const Tutorial = () => {
             key={card._id}
             id={card._id}
             url={card.url}
-            small={
-              card._id !== stepCardClicked && currentStepCompleted && step === 2
-            }
-            onClick={(clickEvent) => handleCardClick(clickEvent, card._id)}
+            onClick={(clickEvent) => handleCardClick(clickEvent, card)}
           />
         ))}
       </div>
@@ -127,12 +159,16 @@ const Tutorial = () => {
           </>
         )}
         {step == 4 && (
-          <button className="primary" onClick={() => setShowTutorial(false)}>
+          <button
+            className="primary"
+            onClick={() => setShowTutorial(false)}
+            disabled={!currentStepCompleted}
+          >
             FINISH TUTORIAL
           </button>
         )}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
