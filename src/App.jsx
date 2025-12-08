bb02184d3842621f5b9917b8d46939736f18eff9
ErickBGomez <erickbgomez.dev@gmail.com>
2025-12-07 21:19:03 -0600

    Add particles background

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import { useContext } from "react";
 import Game from "./components/Game/Game";
 import MainMenu from "./components/MainMenu/MainMenu";
 import GameSettingsContext from "./contexts/GameSettingsContext";
+import Particles from "./components/Particles/Particles";
 
 const App = () => {
   const { difficulty, setDifficulty } = useContext(GameSettingsContext);
 
   return (
-    <>
-      {difficulty >= 0 ? (
-        <Game difficulty={difficulty} returnMenu={setDifficulty} />
-      ) : (
-        <MainMenu setDifficulty={setDifficulty} />
-      )}
-    </>
+    <div className="relative w-full h-screen flex flex-col items-center justify-center">
+      {/* Background */}
+      <Particles
+        particleCount={150}
+        particleSpread={15}
+        moveParticlesOnHover={false}
+      />
+      <div className="inset-0 absolute w-screen h-screen flex flex-col items-center justify-center">
+        {difficulty >= 0 ? (
+          <Game difficulty={difficulty} returnMenu={setDifficulty} />
+        ) : (
+          <MainMenu setDifficulty={setDifficulty} />
+        )}
+      </div>
+    </div>
   );
 };
 
